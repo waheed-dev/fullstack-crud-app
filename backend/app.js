1c9004f0cedde6d336329fa67da3fa41e4380923
abdul waheed <a.waheed3738@gmail.com>
2022-05-07 00:09:48 +0500

    refactored connet and added dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,21 @@
-require('./db/connection')
+
 const express = require('express')
 const app = express()
 const tasks = require('../backend/routes/getAllTasksRoute')
+const connectDB = require('./db/connection')
+require('dotenv').config()
 app.use(express.json())
 app.use(tasks)
+const uri = process.env.MONGO_URI
 
-app.listen(5000,() => {
-    console.log('running');
-})
+const start = async () => {
+    try {
+        await connectDB(uri)
+        app.listen(5000,() => {
+            console.log('running');
+        })
+    } catch (err) {
+console.log(err);
+    }
+}
+start()
