0ab64f21f44af1566f5676e6f968c4a4ed378b55
abdul waheed <a.waheed3738@gmail.com>
2022-05-23 22:43:41 +0500

    added findone query

--- backend/controllers/tasksController.js
+++ backend/controllers/tasksController.js
@@ -1,3 +1,4 @@
+
 const Task = require('../models/task')
 const getTasks = async (req,res) => {
     try {
@@ -8,8 +9,17 @@ const getTasks = async (req,res) => {
     }
 
 }
-const singleTask = (req,res) => {
-    res.json({id :req.params.id})
+const singleTask = async (req,res) => {
+    try {
+        const FindOne = await Task.findOne({_id : req.params.id})
+        res.status(200).json({res : FindOne})
+        if (!FindOne) {
+            res.status(404).send('no task with the matching id found')
+        }
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+
 }
 const insertTask = async (req,res) => {
     try {
